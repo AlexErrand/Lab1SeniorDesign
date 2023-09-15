3a646cdf69bf25a451b8dcee25b9aff2a8ef2633
AzulitePoke <71411978+AlexErrand@users.noreply.github.com>
2023-09-15 13:39:43 -0500

    Bluetooth established and working, connection continues to time out

--- src/Bluetooth.java
+++ src/Bluetooth.java
@@ -110,6 +110,7 @@ public class Bluetooth {
 
         os.write("1".getBytes()); //just send '1' to the device
         os.close();
+        System.out.println(is.read());
         is.close();
         streamConnection.close();
     }
