a08fab702912466ca21a105f36a6f2fa431182ac
AzulitePoke <71411978+AlexErrand@users.noreply.github.com>
2023-09-22 12:45:50 -0500

    bug changes

--- src/RealTimeGraphDemo.java
+++ src/RealTimeGraphDemo.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.jfree.chart.ChartFactory;
@@ -15,29 +16,39 @@ import org.jfree.data.xy.XYSeriesCollection;
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortEvent;
 import com.fazecast.jSerialComm.SerialPortDataListener;
+
 import javax.swing.*;
 
 public class RealTimeGraphDemo extends JFrame {
 
     private final XYSeries dataSeries;
+    private final XYSeries dataSeriesF;
     private final SerialPort arduinoPort;
     private final JButton toggleButton;
+    private final JButton toggleTemp;
     private final JLabel temperatureDisplay;
     private final JLabel dataStatus;
     private boolean sensorOn = true;
+    private boolean enableDataStream = false;
     private int xCounter = 0;
     private final int maxDataPoints = 300;
     private boolean isDataReceived = false;
     private double previousTemperature = Double.NaN;
-    private final long lastUpdateTimestamp = 0;
+    private String tempLabel = " C";
+
+    private boolean isArduinoConnected = true; // Track Arduino connection state
 
     private boolean axisChangedFlag = false; // Flag to prevent reentry
+
+    private boolean isTemperatureInFahrenheit = false; // Flag to track temperature scale
 
     public RealTimeGraphDemo(final String title) {
         super(title);
 
         dataSeries = new XYSeries("Temperature Data");
+        dataSeriesF = new XYSeries("Temp Data F");
         XYSeriesCollection dataset = new XYSeriesCollection(dataSeries);
+        XYSeriesCollection datasetF = new XYSeriesCollection(dataSeriesF);
 
         JFreeChart chart = createChart(dataset);
         ChartPanel chartPanel = new ChartPanel(chart, true, true, true, true, true);
@@ -46,20 +57,57 @@ public class RealTimeGraphDemo extends JFrame {
 
         chartPanel.setSize(400, 400);
 
+        JFreeChart chartF = createChart(datasetF);
+        ChartPanel chartPanelF = new ChartPanel(chartF, true, true, true, true, true);
+        chartPanelF.setMouseWheelEnabled(true);
+        chartPanelF.setZoomTriggerDistance(20);
+        chartPanelF.setBounds(10,10,400,400);
+
         dataStatus = new JLabel();
         temperatureDisplay = new JLabel();
-        dataStatus.setText("Not Receiving values");
-        temperatureDisplay.setText("0° Celsius");
+        dataStatus.setText("Status: Not Receiving Values");
+        temperatureDisplay.setText("Temperature: 0° Celsius");
 
         setLayout(null);
 
         chartPanel.setBounds(10, 10, 400, 400);
-        dataStatus.setBounds(750, 10, 200, 20);
-        temperatureDisplay.setBounds(10, 420, 200, 20);
 
+        temperatureDisplay.setBounds(500, 100, 400, 100);
+        temperatureDisplay.setFont(new Font("serif", Font.PLAIN, 24));
+        temperatureDisplay.setForeground(Color.BLUE);
+
+        dataStatus.setBounds(500, 10, 400, 100);
+        dataStatus.setFont(new Font("serif", Font.PLAIN, 24));
+        dataStatus.setForeground(Color.RED);
+
+        add(chartPanelF);
         add(chartPanel);
         add(dataStatus);
         add(temperatureDisplay);
+        toggleTemp = new JButton("Change Temperature to Fahrenheit");
+        toggleTemp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (isTemperatureInFahrenheit) {
+                    isTemperatureInFahrenheit = false;
+                    toggleTemp.setText("Change Temperature to Celsius");
+                    chartPanelF.setVisible(false);
+                    chartPanel.setVisible(true);
+                    tempLabel = " C";
+                    //arduinoPort.closePort();
+                } else {
+                    isTemperatureInFahrenheit = true;
+                    toggleTemp.setText("Change Temperature to Fahrenheit");
+                    chartPanelF.setVisible(true);
+                    chartPanel.setVisible(false);
+                    tempLabel  = " F";
+                    //arduinoPort.openPort();
+                }
+            }
+        });
+        toggleTemp.setBounds(430, 300, 400, 30);
+        add(toggleTemp);
 
         toggleButton = new JButton("Toggle Sensor");
         toggleButton.addActionListener(new ActionListener() {
@@ -72,6 +120,7 @@ public class RealTimeGraphDemo extends JFrame {
         add(toggleButton);
 
         arduinoPort = SerialPort.getCommPort("COM3");
+
         arduinoPort.openPort();
         arduinoPort.setBaudRate(115200);
 
@@ -85,30 +134,30 @@ public class RealTimeGraphDemo extends JFrame {
 
             @Override
             public void serialEvent(SerialPortEvent event) {
-                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE || !sensorOn)
-                    dataStatus.setText("Not Receiving Data");
-                else {
-                    dataStatus.setText("Receiving Data");
-                }
-
                 byte[] newData = new byte[arduinoPort.bytesAvailable()];
                 int numRead = arduinoPort.readBytes(newData, newData.length);
                 String receivedData = new String(newData);
 
                 try {
                     double temperature = Double.parseDouble(receivedData.trim());
+                    System.out.println(temperature);
 
                     if (isValidTemperature(temperature) && isValidTemperatureDeviation(temperature)) {
                         long currentTimestamp = System.currentTimeMillis();
 
                         if (currentTimestamp - lastUpdateTimestamp.get() >= 1000) {
                             SwingUtilities.invokeLater(() -> {
-                                isDataReceived = true;
+
                                 dataSeries.add(xCounter, temperature);
+                                dataSeriesF.add(xCounter,(temperature*1.8)+32);
+
                                 if (xCounter >= maxDataPoints) {
                                     dataSeries.remove(0);
+                                    dataSeriesF.remove(0);
                                 }
-                                temperatureDisplay.setText(String.valueOf(temperature));
+
+                                temperatureDisplay.setText(temperature + tempLabel);
+
                                 xCounter++;
                                 previousTemperature = temperature;
                                 lastUpdateTimestamp.set(currentTimestamp);
@@ -135,20 +184,6 @@ public class RealTimeGraphDemo extends JFrame {
                 }
             }
         });
-
-        Timer timer = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (sensorOn) {
-                    if (!isDataReceived) {
-                        dataSeries.add(xCounter, Double.NaN);
-                    }
-                    xCounter++;
-                    isDataReceived = false;
-                }
-            }
-        });
-        timer.start();
     }
 
     private JFreeChart createChart(final XYSeriesCollection dataset) {
@@ -194,6 +229,36 @@ public class RealTimeGraphDemo extends JFrame {
         }
         double deviation = Math.abs(temperature - previousTemperature);
         return deviation <= temperatureDeviationThreshold;
+    }
+
+    private void temperatureConvert2Celsius() {
+        if (isTemperatureInFahrenheit) {
+            for (int i = 0; i < dataSeries.getItemCount(); i++) {
+                double celsius = (dataSeries.getY(i).doubleValue() - 32) * 5/9;
+                dataSeries.updateByIndex(i, celsius);
+            }
+            isTemperatureInFahrenheit = false;
+            // Update the y-axis label
+            updateYAxisLabel("Temperature (°C)");
+        }
+    }
+
+    private void temperatureConvert2Fht() {
+        if (!isTemperatureInFahrenheit) {
+            for (int i = 0; i < dataSeries.getItemCount(); i++) {
+                double fahrenheit = dataSeries.getY(i).doubleValue() * 9/5 + 32;
+                dataSeries.updateByIndex(i, fahrenheit);
+            }
+            isTemperatureInFahrenheit = true;
+            // Update the y-axis label
+            updateYAxisLabel("Temperature (°F)");
+        }
+    }
+
+    private void updateYAxisLabel(String label) {
+        JFreeChart chart = (JFreeChart) ((ChartPanel) getContentPane().getComponent(0)).getChart();
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.getRangeAxis().setLabel(label);
     }
 
     public static void main(final String[] args) {
